b080dd4b69f61d428b58935432d8e94fcf0e137a
mallabonikki <mallabonikki6678@gmail.com>
2017-04-01 15:05:31 +1100

    Created Controller, View and the instance of a Movie model

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            //instance of movie model
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //pass the instance of the movie to return to the view
+            return View(movie);
+        }
+
+
+    }
+}
