32d77360feb2259ad014a9323cfaecf5bffba657
mallabonikki <mallabonikki6678@gmail.com>
2017-04-16 14:07:03 +1000

    Adding and Changing a Model for migration

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,6 +8,9 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; } 
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; } //Navigation Property - use for relational model
+        public byte MembershipTypeId { get; set; } //Foreign Key for MembershipType
     }
 }
