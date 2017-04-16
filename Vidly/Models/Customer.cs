e5beda331271ee0c59010f222eaef46de3f743ef
mallabonikki <mallabonikki6678@gmail.com>
2017-04-16 22:08:52 +1000

    Overriding Conventions, changing database contraits through add-migration

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required] //DataAnnotation or Attribute
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; } //Navigation Property - use for relational model
