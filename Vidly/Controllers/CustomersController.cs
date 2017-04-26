a4d41e7dc3ee2a9adaa0fc98d24647435a6a4dbb
mallabonikki <mallabonikki6678@gmail.com>
2017-04-26 12:56:53 +1000

    Building Forms, The Markup

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -22,6 +22,11 @@ namespace Vidly.Controllers
             _context.Dispose(); //disposing objects
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
