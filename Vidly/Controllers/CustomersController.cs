4cd36dd6d2099eee6681a900628bdb1ff6334424
mallabonikki <mallabonikki6678@gmail.com>
2017-04-29 19:07:24 +1000

    Adding Validation, Put Required to Data Annotation, use ValidationMessageFor

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -38,6 +38,17 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            //Add validations
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
             //check the if the customer has an Id otherwise it's a new customer
             if (customer.Id == 0)
                 _context.Customers.Add(customer); //New customer
