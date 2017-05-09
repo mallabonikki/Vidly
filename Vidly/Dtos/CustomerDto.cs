55d70352bed011d8d6117d26ef72369216f7355c
mallabonikki <mallabonikki6678@gmail.com>
2017-05-09 22:22:42 +1000

    Create a Web API for Customers and add code comments

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -10,8 +10,6 @@ namespace Vidly.Dtos
     {
         public int Id { get; set; }
 
-        [Required] //DataAnnotation or Attribute
-        [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsLetter { get; set; }
