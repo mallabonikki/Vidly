36ea2f9075166dd98196776ff256d8cfda014606
mallabonikki <mallabonikki6678@gmail.com>
2017-05-17 18:44:55 +1000

    Domain Modelling - Adding Rental Domain Model

---
+++ Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<GenreType> GenreTypes { get; set; }
+        public DbSet<Rental> Rentals { get; set; } // Plural, create a Rentals table in the database using Dbset<Model>, Model=Rental, ready for migration
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
