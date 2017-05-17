44ce7f288c3a2d571e8c52d74e44dd6295b96ebe
mallabonikki <mallabonikki6678@gmail.com>
2017-05-17 16:10:22 +1000

    Create an API New Rental use case, start on the back-end, Understanding the problem

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost] // Input of our application
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
