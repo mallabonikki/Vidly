66c19fcd71c56c1b1fd0d5bf25d58ad505fe814e
mallabonikki <mallabonikki6678@gmail.com>
2017-05-24 10:13:17 +1000

    Customer Movie validation and ease use

--- Vidly/ViewModels/CustomerFormViewModel.cs
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -10,5 +10,13 @@ namespace Vidly.ViewModels
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; } //plural as iteration, collection
         public Customer Customer { get; set; } //re-use from the domain model reference
+
+        public string TitleCustomer
+        {
+            get
+            {
+                return (Customer.Id != 0) ? "Edit Customer" : "New Customer";
+            }
+        }
     }
 }
